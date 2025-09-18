40ec4665cf9fbfc2e29f40f107ab9be150ec849e
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-09-18 12:46:32 +0800

    Feat: Addjust pool SSL for Render deployment

--- model/pool.js
+++ model/pool.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  SSL: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
